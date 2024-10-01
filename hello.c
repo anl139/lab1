da67cf81c1093a704fe25e855601b7398a13d5b9
Andrew Lam <anl139@ucsd.edu>
2024-10-01 22:34:33 +0000

    asdfasdf

--- hello.c
+++ hello.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include <helper.c>
 int main() {
   char hello[] = "Hello!"; 
   char hello2[] = { 72, 101, 108, 108, 111, 33, 0 };
---
+++ helper.c
@@ -0,0 +1,4 @@
+#include <stdio.h>
+int sums(){
+    return 1+2;
+}
